c74348a53a37525df9cbc2fa4c5cc52a829ff4b9
Vinod <vinod.jat@oraneconsulting.com>
2017-11-11 19:42:35 +0530

    first commit

---
+++ src/app/user.js
@@ -0,0 +1,14 @@
+import React ,{Component}  from 'react';
+
+class user extends Component {
+    render(){
+
+        return(
+            <div className="app">
+            My First React app
+            </div>
+        )
+    }
+
+}
+exports user;
